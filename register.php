b2cd96711903cf8c311a6f6b7b205597dbdf0289
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-18 23:11:34 -0700

    register

--- register.php
+++ register.php
@@ -9,7 +9,16 @@
     <div id="inputContainer"> 
         <form id="loginForm" action="regiser.php" method="POST">
             <h2>Login to your Account</h2>
-            <input id="loginUserName" name="loginUserName" type="text" placeHolder="e.g. Dimebag Darrell" required> 
+            <p>
+                <label for="loginUserName">Username: </label>
+                <input id="loginUserName" name="loginUserName" type="text" placeHolder="e.g. Dimebag Darrell" required>
+            </p>
+            <p> 
+                <label for="loginPassword">Password: </label>
+                <input type="password" id="loginPassword" name="loginPassword" required>
+            </p>
+
+            <button type="submit" name="loginButton">Log In</button>
         </form>
     </div>
 </body>
