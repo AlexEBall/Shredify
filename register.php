b475450e96193587b2e7b30211946c4f0b6fa3a5
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-18 23:06:06 -0700

    intial commit

---
+++ register.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Shredify</title>
+</head>
+<body>
+    <div id="inputContainer"> 
+        <form id="loginForm" action="regiser.php" method="POST">
+            <h2>Login to your Account</h2>
+            <input id="loginUserName" name="loginUserName" type="text" placeHolder="e.g. Dimebag Darrell" required> 
+        </form>
+    </div>
+</body>
+</html>
