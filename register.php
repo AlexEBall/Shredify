c3d4d0880357c433714f67337d41539ba50f424f
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-19 21:03:35 -0700

    adding php

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -19,6 +24,40 @@
             </p>
 
             <button type="submit" name="loginButton">Log In</button>
+        </form>
+
+        <form id="registerForm" action="regiser.php" method="POST">
+            <h2>Create your Free Account</h2>
+            <p>
+                <label for="userName">Username: </label>
+                <input id="userName" name="userName" type="text" placeHolder="e.g. Dimebag Darrell" required>
+            </p>
+            <p>
+                <label for="firstName">First Name: </label>
+                <input id="firstName" name="firstName" type="text" placeHolder="Dave" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name: </label>
+                <input id="lastName" name="lastName" type="text" placeHolder="Williams" required>
+            </p>
+            <p>
+                <label for="email">Email: </label>
+                <input id="email" name="email" type="text" placeHolder="cowboysfromhell@gmail.com" required>
+            </p>
+            <p>
+                <label for="email2">Confirm Email: </label>
+                <input id="email2" name="email2" type="text" placeHolder="cowboysfromhell@gmail.com" required>
+            </p>
+            <p> 
+                <label for="password">Password: </label>
+                <input type="password" id="password" name="password" required>
+            </p>
+            <p> 
+                <label for="password2">Confirm Password: </label>
+                <input type="password2" id="password2" name="password" required>
+            </p>
+
+            <button type="submit" name="registerButton">Sign Up</button>
         </form>
     </div>
 </body>
