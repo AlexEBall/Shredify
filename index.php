75357d07617cb35a759dcef0dce21e82b3348cf9
Alex Edward Ball <ballalexedward@gmail.com>
2018-08-27 18:30:36 -0700

    modularizing index.php and adding items to the db

--- index.php
+++ index.php
@@ -25,33 +25,7 @@
     <div class="mainContainer">
 
         <div class="topContainer">
-            <div class="navBarContainer">
-                <div class="navBar">
-                    <a href="index.php" class="logo">
-                        <img src="assets/images/icons/icons8-lightning_bolt.png" alt="Site Logo" />
-                    </a>
-
-                    <div class="group">
-                        <div class="navItem">
-                            <a class="navItemLink" href="search.php">Search
-                                <img class="icon" src="assets/images/icons/icons8-search.png" alt="Search Button">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="group">
-                        <div class="navItem">
-                            <a class="navItemLink" href="browse.php">Browse</a>
-                        </div>
-                        <div class="navItem">
-                            <a class="navItemLink" href="yourMusic.php">Your Shredders</a>
-                        </div>
-                        <div class="navItem">
-                            <a class="navItemLink" href="profile.php">Vic Rattlehead</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            <?php include("includes/navBarContainer.php"); ?>
         </div>
 
         <div class="nowPlayingBarContainer">
